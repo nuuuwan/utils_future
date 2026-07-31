6550e60a992643c0d2a5ab6c0b6e75a642080f15
nuuuwan <nuuuwan@gmail.com>
2026-07-31 18:08:13 +0530

    Add YAMLFile class for reading and writing YAML files

---
+++ src/utils_future/file/YAMLFile.py
@@ -0,0 +1,15 @@
+import yaml
+
+from utils_future.file.File import File
+
+
+class YAMLFile(File):
+    ENCODING = "utf-8"
+
+    def read(self):
+        with open(self.path, "r", encoding=self.ENCODING) as f:
+            return yaml.safe_load(f)
+
+    def write(self, content):
+        with open(self.path, "w", encoding=self.ENCODING) as f:
+            yaml.safe_dump(content, f, allow_unicode=True, sort_keys=False)
